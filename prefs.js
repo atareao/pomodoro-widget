daab4a99f44d8d939b618ba4fcdb8036240c105f
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2024-05-28 07:21:32 +0200

    sub divide

--- prefs.js
+++ prefs.js
@@ -40,24 +40,74 @@ export default class PomodoroWidgetPreferences extends ExtensionPreferences{
     page.set_title(_("Pomodoro Widget"));
     page.set_name("pomodoro-widget-preferences");
 
+    page.add(this.buildPomodorosGroup(window));
+    page.add(this.buildPomodorosLengthGroup(window));
+    page.add(this.buildPomodorosAutoStartGroup(window));
+    window.add(page);
+
+    const page_colors = Adw.PreferencesPage.new();
+    page_colors.set_title(_("Colors"))
+    window.add(page_colors);
+  }
+
+  buildPomodorosGroup(window){
     const group = Adw.PreferencesGroup.new();
     group.set_title(_("Pomodoros"));
-    group.set_name("pomodoros_group");
+    group.set_name("pomodoros-group");
 
     const number_of_pomodoros = Adw.SpinRow.new_with_range(1, 10, 1);
     number_of_pomodoros.set_title(_("Number of pomodoros"));
     number_of_pomodoros.set_name("number-of-pomodoros");
-    window._settings.bind("pomodoros", number_of_pomodoros, "active", Gio.SettingsBindFlags.DEFAULT);
+    window._settings.bind("pomodoros", number_of_pomodoros, "value", Gio.SettingsBindFlags.DEFAULT);
     group.add(number_of_pomodoros);
+    return group;
+  }
+
+  buildPomodorosLengthGroup(window){
+    const group = Adw.PreferencesGroup.new();
+    group.set_title(_("Length"));
+    group.set_name("pomodoros-length-group");
 
     const pomodoro_length = Adw.SpinRow.new_with_range(1, 60, 1);
     pomodoro_length.set_title(_("Pomodoro length"));
     pomodoro_length.set_name("pomodoro-length");
-    window._settings.bind("pomodoro-length", pomodoro_length, "active", Gio.SettingsBindFlags.DEFAULT);
+    window._settings.bind("pomodoro-length", pomodoro_length, "value", Gio.SettingsBindFlags.DEFAULT);
     group.add(pomodoro_length);
 
-    page.add(group);
-    window.add(page);
+    const short_break_length = Adw.SpinRow.new_with_range(1, 60, 1);
+    short_break_length.set_title(_("Short break length"));
+    short_break_length.set_name("short-break-length");
+    window._settings.bind("short-break-length", short_break_length, "value", Gio.SettingsBindFlags.DEFAULT);
+    group.add(short_break_length);
+
+    const long_break_length = Adw.SpinRow.new_with_range(1, 60, 1);
+    long_break_length.set_title(_("Long break length"));
+    long_break_length.set_name("long-break-length");
+    window._settings.bind("long-break-length", long_break_length, "value", Gio.SettingsBindFlags.DEFAULT);
+    group.add(long_break_length);
+
+    return group;
   }
+
+  buildPomodorosAutoStartGroup(window){
+    const group = Adw.PreferencesGroup.new();
+    group.set_title(_("Auto start"));
+    group.set_name("pomodoros-auto-start-group");
+
+    const auto_start_breaks = Adw.SwitchRow.new();
+    auto_start_breaks.set_title(_("Auto start breaks"));
+    auto_start_breaks.set_name("auto-start-breaks");
+    window._settings.bind("auto-start-breaks", auto_start_breaks, "active", Gio.SettingsBindFlags.DEFAULT);
+    group.add(auto_start_breaks);
+
+    const auto_start_pomodoros = Adw.SwitchRow.new();
+    auto_start_pomodoros.set_title(_("Auto start pomodoros"));
+    auto_start_pomodoros.set_name("auto-start-pomodoros");
+    window._settings.bind("auto-start-pomodoros", auto_start_pomodoros, "active", Gio.SettingsBindFlags.DEFAULT);
+    group.add(auto_start_pomodoros);
+
+    return group;
+  }
+
 };
 
