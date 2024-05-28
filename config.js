fa6096346fd646a438a035dfadbdb4ac12f7f3e2
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2024-05-28 06:45:51 +0200

    configuration

---
+++ config.js
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: GSConnect Developers https://github.com/GSConnect
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+export default {
+    PACKAGE_VERSION: 57,
+    PACKAGE_URL: 'https://github.com/atareao/pomodoro-widget',
+    PACKAGE_BUGREPORT: 'https://github.com/atareao/pomodoro-widget/issues/new',
+    PACKAGE_DATADIR: '/usr/local/share/gnome-shell/extensions/pomodoro-widget@atareao.es',
+    PACKAGE_LOCALEDIR: '/usr/local/share/locale',
+    GSETTINGS_SCHEMA_DIR: '/usr/local/share/glib-2.0/schemas',
+    GNOME_SHELL_LIBDIR: '/usr/local/lib64',
+
+    APP_ID: 'org.gnome.shell.extensions.pomodoro-widget',
+    APP_PATH: '/org/gnome/shell/extensions/pomodoro-widget',
+
+    IS_USER: false,
+
+    // External binary paths
+};
